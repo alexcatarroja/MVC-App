be5309fe465b2b01da6704bfd8ae9be150e1557a
Acatarro <alexcatarroja98@outlook.com>
2021-11-16 11:21:51 -0800

    Initial Commit

---
+++ finaltest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(finaltest.Startup))]
+namespace finaltest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
